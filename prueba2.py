bf3156a221287d7650eca54d82a58b171abc9a47
CKL0 <luiscamilooc@gmail.com>
2023-05-13 18:32:19 -0600

    Tratando de separar esto en una matriz

--- prueba2.py
+++ prueba2.py
@@ -3,26 +3,32 @@ import sys
 from pathlib import Path
 sys.setrecursionlimit(10000000)
 
-mazo_global = open("/home/camilo/Downloads/Mazo.txt", encoding="utf-8")
+with open("Mazo.txt", "r", encoding="utf-8") as archivo:
+    contenido = archivo.read().rstrip("\n")
+
+# mazo_global = open("/home/camilo/Downloads/Mazo.txt", encoding="utf-8")
 
 def iniciar_juego():
     """Funcion que inicia el juego"""
-    mazo = cargar_mazo(mazo_global, '', [])
+    mazo = separar_mazo_matriz(contenido, 0, len(contenido), '', [])
 
-def cargar_mazo(mazo_global, texto_actual, texto_secciones):
-    contenido_mazo_linea = mazo_global.readline()
-    if contenido_mazo_linea != '':
-        return separar_mazo_matriz(contenido_mazo_linea, 0, len(contenido_mazo_linea), texto_actual, texto_secciones)
-    else:
-        return texto_secciones
 def separar_mazo_matriz(contenido, indice, largo, texto_actual, texto_secciones):
     '''Funcion que separar el mazo en contenido'''
-
     if indice == largo:
-        return texto_secciones
+        # return remover_saltos_linea(contenido, 0, len(contenido), [])
+        return remover_saltos_linea(texto_secciones, 0, len(texto_secciones), [])
     elif contenido[indice] == "/":
         return separar_mazo_matriz(contenido, indice + 1, largo, '', texto_secciones + [texto_actual])
     else:
         return separar_mazo_matriz(contenido, indice + 1, largo, texto_actual + contenido[indice], texto_secciones)
     
-print(cargar_mazo(mazo_global, '', []))
+def remover_saltos_linea(lista_contenido, indice, largo, resultado):
+    '''Funcion que trata de remover los \n que hay en la lista'''
+    if indice == largo:
+        return resultado
+    elif lista_contenido[indice] == "\n":
+        return remover_saltos_linea(lista_contenido, indice + 1, largo, resultado)
+    else:
+        return remover_saltos_linea(lista_contenido, indice + 1, largo, resultado + [lista_contenido[indice]])
+    
+print(separar_mazo_matriz(contenido, 0, len(contenido), '', []))
