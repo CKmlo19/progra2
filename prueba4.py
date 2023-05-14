b096a341daa94d940253f16c69f944ea24bc7f4e
CKL0 <luiscamilooc@gmail.com>
2023-05-14 12:06:37 -0600

    Implementando la mano aleatoria con el mazo

---
+++ prueba4.py
@@ -0,0 +1,70 @@
+import os 
+import sys
+import random
+from pathlib import Path
+sys.setrecursionlimit(10000000)
+
+# esto abre el archivo que esta en la misma carpeta
+with open("Mazo.txt", "r", encoding="utf-8") as archivo:
+    contenido = archivo.read().rstrip("\n")
+
+# mazo_global = open("/home/camilo/Downloads/Mazo.txt", encoding="utf-8")
+
+def iniciar_juego():
+    """Funcion que inicia el juego"""
+    mazo = separar_mazo_matriz(contenido, 0, len(contenido), '', [])
+    mano = manos(mazo)
+    return mano
+
+def separar_mazo_matriz(contenido, indice, largo, texto_actual, texto_secciones):
+    '''Funcion que separar el mazo de acuerdo a un caracter separador previamente en el archivo'''
+    if indice == largo:
+        # return texto_secciones
+        return remover_saltos_linea(texto_secciones, 0, len(texto_secciones), [])
+    elif contenido[indice] == "/":
+        return separar_mazo_matriz(contenido, indice + 1, largo, '', texto_secciones + [texto_actual])
+    else:
+        return separar_mazo_matriz(contenido, indice + 1, largo, texto_actual + contenido[indice], texto_secciones)
+    
+def remover_saltos_linea(lista_contenido, indice, largo, resultado):
+    '''Funcion que trata de remover los \n que hay en la lista'''
+    if indice == largo:
+        return resultado
+    elif lista_contenido[indice] == "\n":
+        return remover_saltos_linea(lista_contenido, indice + 1, largo, resultado)
+    elif lista_contenido[indice] == " \n":
+        return remover_saltos_linea(lista_contenido, indice + 1, largo, resultado)
+    else:
+        return remover_saltos_linea(lista_contenido, indice + 1, largo, resultado + [lista_contenido[indice]])
+
+def eliminar_elemento(lista, elemento):
+    ''' Elimina un elemento de una lista
+    '''
+    if type(lista) != list:
+        return 'Error01'
+    else:
+        return eliminar_elemento_aux(lista, elemento, 0, len(lista), [])
+
+def eliminar_elemento_aux(lista, elemento, indice, largo, resultado):
+    ''' Funcion auxiliar 
+    '''
+    if largo <= indice:
+        return resultado
+    elif lista[indice] == elemento:
+        return eliminar_elemento_aux(lista, elemento, indice + 1, largo, resultado)
+    else:
+        return eliminar_elemento_aux(lista, elemento, indice + 1, largo, resultado + [lista[indice]])
+
+def manos(mazo):
+    tamaño = 3
+    mano1 = random.sample(mazo, tamaño)
+    nueva_lista = eliminar_elemento(mazo, mano1[0])
+    nueva_lista = eliminar_elemento(nueva_lista, mano1[1])
+    nueva_lista = eliminar_elemento(nueva_lista, mano1[2])
+    mano2 = random.sample(nueva_lista, tamaño)
+    nueva_lista = eliminar_elemento(nueva_lista, mano2[0])
+    nueva_lista = eliminar_elemento(nueva_lista, mano2[1])
+    nueva_lista = eliminar_elemento(nueva_lista, mano2[2])
+    return [mano1, mano2, nueva_lista]
+    
+print(iniciar_juego())
