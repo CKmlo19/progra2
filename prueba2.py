4805e3ddce2c69f62da502ec17162786a55049d5
CKL0 <luiscamilooc@gmail.com>
2023-05-13 18:36:35 -0600

    Al fin logre separarlo en filas

--- prueba2.py
+++ prueba2.py
@@ -11,11 +11,12 @@ with open("Mazo.txt", "r", encoding="utf-8") as archivo:
 def iniciar_juego():
     """Funcion que inicia el juego"""
     mazo = separar_mazo_matriz(contenido, 0, len(contenido), '', [])
+    return len(mazo)
 
 def separar_mazo_matriz(contenido, indice, largo, texto_actual, texto_secciones):
     '''Funcion que separar el mazo en contenido'''
     if indice == largo:
-        # return remover_saltos_linea(contenido, 0, len(contenido), [])
+        # return texto_secciones
         return remover_saltos_linea(texto_secciones, 0, len(texto_secciones), [])
     elif contenido[indice] == "/":
         return separar_mazo_matriz(contenido, indice + 1, largo, '', texto_secciones + [texto_actual])
@@ -29,6 +30,6 @@ def remover_saltos_linea(lista_contenido, indice, largo, resultado):
     elif lista_contenido[indice] == "\n":
         return remover_saltos_linea(lista_contenido, indice + 1, largo, resultado)
     else:
-        return remover_saltos_linea(lista_contenido, indice + 1, largo, resultado + [lista_contenido[indice]])
+        return remover_saltos_linea(lista_contenido, indice + 1, largo, resultado + [[lista_contenido[indice]]])
     
-print(separar_mazo_matriz(contenido, 0, len(contenido), '', []))
+print(iniciar_juego())
