9683bdaab36526e2776f065b6462faa2a04e9b84
CKL0 <luiscamilooc@gmail.com>
2023-05-17 17:04:24 -0600

    Agregando un archivo para probar colores ANSI

---
+++ colores_ANSI.py
@@ -0,0 +1,8 @@
+rojo_negrita = "\033[1;91m"
+verde_subrayado = "\033[4;92m"
+azul_fondo_blanco = "\033[44;97m"
+resetear = "\033[0m"
+
+print(rojo_negrita + "Texto en rojo y negrita" + resetear)
+print(verde_subrayado + "Texto en verde y subrayado" + resetear)
+print(azul_fondo_blanco + "Texto con fondo azul y letras blancas" + resetear)
